324f30d2a9d2136d5303c6595abe9c19c5bd0ac9
angelomelo <angelorobsonmelo@gmail.com>
2019-02-04 23:21:06 -0300

    Refactor code

--- app/src/main/java/com/angelomelo/alternative/application/modules/events/events/eventDetail/ui/eventdetail/EventDetailViewModel.kt
+++ app/src/main/java/com/angelomelo/alternative/application/modules/events/events/eventDetail/ui/eventdetail/EventDetailViewModel.kt
@@ -2,35 +2,28 @@ package com.angelomelo.alternative.application.modules.events.events.eventDetail
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.angelomelo.alternative.application.UseCase
 import com.angelomelo.alternative.application.domain.Event
-import com.angelomelo.alternative.application.injections.InjectionUseCase.Companion.provideGetEventDetail
+import com.angelomelo.alternative.application.injections.InjectionApiDataSource
 import com.angelomelo.alternative.application.service.remote.commom.ResponseBase
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class EventDetailViewModel : ViewModel() {
 
-    private val mGetEventDetail = provideGetEventDetail()
+    private val mEventDatailApiDataSource = InjectionApiDataSource.provideEventDetailApiDataSource()
 
     var eventsResponse = MutableLiveData<ResponseBase<Event>>()
     var messageEmpty = MutableLiveData<String>()
     var error = MutableLiveData<String>()
 
     fun findOne(id: Long) {
-        mGetEventDetail.findOne(id, object : UseCase.LoadUseCaseCallback<ResponseBase<Event>> {
-
-            override fun onLoaded(response: ResponseBase<Event>) {
-                eventsResponse.value = response
-            }
-
-            override fun onEmptyData() {
-                messageEmpty.value = "Value is empty"
-            }
-
-            override fun onFailed(errorDescription: String) {
-                error.value = errorDescription
-            }
-
-        })
+        mEventDatailApiDataSource.findOne(id)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { result -> eventsResponse.value = result },
+                { error -> this.error.value = error.localizedMessage  }
+            )
     }
 
 }
--- app/src/main/java/com/angelomelo/alternative/application/modules/events/events/ui/event/EventViewModel.kt
+++ app/src/main/java/com/angelomelo/alternative/application/modules/events/events/ui/event/EventViewModel.kt
@@ -2,37 +2,37 @@ package com.angelomelo.alternative.application.modules.events.events.ui.event
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.angelomelo.alternative.application.UseCase
 import com.angelomelo.alternative.application.domain.Event
 import com.angelomelo.alternative.application.domain.filter.EventFilter
-import com.angelomelo.alternative.application.injections.InjectionUseCase.Companion.provideGetEvents
+import com.angelomelo.alternative.application.injections.InjectionApiDataSource
 import com.angelomelo.alternative.application.service.remote.commom.ResponseListBase
-import com.angelomelo.alternative.application.usecases.remote.event.GetEvents
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class EventViewModel : ViewModel() {
 
-    private val mGetEvents: GetEvents = provideGetEvents()
+    private val mEventApiDataSource = InjectionApiDataSource.provideEventApiDataSource()
 
     var eventsResponse = MutableLiveData<ResponseListBase<Event>>()
     var messageEmpty = MutableLiveData<String>()
     var error = MutableLiveData<String>()
 
     fun getEvents(filter: EventFilter, page: Int?) {
-        mGetEvents.getEvents(filter, page, object : UseCase.LoadUseCaseCallback<ResponseListBase<Event>> {
+        mEventApiDataSource.getEvent(filter, page)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnCompleted {
+                val events = eventsResponse.value?.data?.content
 
-            override fun onLoaded(response: ResponseListBase<Event>) {
-                eventsResponse.value = response
+                if (page == 0 && events?.isEmpty()!!) {
+                    messageEmpty.value = "Empty message"
+                }
+
             }
-
-            override fun onEmptyData() {
-                messageEmpty.value = "Value is empty"
-            }
-
-            override fun onFailed(errorDescription: String) {
-              error.value = errorDescription
-            }
-
-        })
+            .subscribe(
+                { result -> eventsResponse.value = result },
+                { error -> this.error.value = error.localizedMessage  }
+            )
     }
 
 }
