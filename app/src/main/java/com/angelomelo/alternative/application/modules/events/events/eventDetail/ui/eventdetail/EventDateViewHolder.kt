aa6d8022534f6ccdab361fca7b7fb31b7cbdf86f
angelomelo <angelorobsonmelo@gmail.com>
2019-02-04 23:52:12 -0300

    fix contraints dates prices

--- app/src/main/java/com/angelomelo/alternative/application/modules/events/events/eventDetail/ui/eventdetail/EventDateViewHolder.kt
+++ app/src/main/java/com/angelomelo/alternative/application/modules/events/events/eventDetail/ui/eventdetail/EventDateViewHolder.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.angelomelo.alternative.application.domain.EventDate
 import kotlinx.android.synthetic.main.event_date_item.view.*
+import java.text.SimpleDateFormat
 
 class EventDateViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -11,8 +12,14 @@ class EventDateViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val price = itemView.priceTextView
 
     fun bindData(eventDate: EventDate) {
-        date.text  = eventDate.date.toString()
-        price.text = eventDate.price.toString()
+        val pattern = "dd/MM/yyyy"
+        val simpleDateFormat = SimpleDateFormat(pattern)
+
+        val dateFormat = simpleDateFormat.format(eventDate.date)
+
+        println(date)
+        date.text  = dateFormat
+        price.text = "R$"+ eventDate.price.toString()
     }
 
 
